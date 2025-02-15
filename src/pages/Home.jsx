a19b209d4a252ff1cf44459f1e30de33338d6e80
Mdintkhabraza <razaintkhab0@gmail.com>
2025-02-15 18:43:31 +0530

    initill commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Home(){
+return(
+    <>
+       <from>
+        <input type="text"></input>
+        <button>ADD TASK</button>
+       </from>
+    
+    </>
+)
+
+
+}
